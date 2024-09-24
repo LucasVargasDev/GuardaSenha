c59931b3d5e20b9b7dc4f98eb2de66ff0366fff8
Lucas Vargas <lucasvargasjob@gmail.com>
2024-09-24 16:15:48 -0300

    guardaSenha002 - Adicionado Style definido no figma

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      {/* Ajusta a cor da StatusBar para preto */}
+      <StatusBar barStyle="light-content" backgroundColor="#293A97" />
+      
+      {/* Cabeçalho com o título GuardaSenha */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>GuardaSenha</Text>
+      </View>
+      
+      {/* Corpo da tela */}
+      <View style={styles.body}>
+        <Text>Hello World</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#293A97', // Fundo azul
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    justifyContent: 'center',
+  },
+  headerText: {
+    color: '#C8D4F1', // Cor do texto
+    fontSize: 24, // Tamanho da fonte
+    textAlign: 'left', // Texto alinhado à esquerda
+  },
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF', // Fundo branco no corpo
+  },
+});
